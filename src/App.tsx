9482ed6f3e261a2c241979e6cd0420349fd18f09
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-05-04 18:13:42 +0530

    init - project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SearchJobs from './pages/SearchJobs'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="main_heading">Weekday Assignment</h1>
+      <SearchJobs />
+    </>
+  )
+}
+
+export default App
