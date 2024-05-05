ece8d19301dcddc82ebb3ffccc3be25d3ed660a9
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-05-05 19:22:30 +0530

    config driven UI for exp, salary, location

---
+++ src/components/SingleSelectConfig.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { useDispatch } from 'react-redux';
+import { setExperience, setMinimumBasePaySalary } from '../slice/jobSlice';
+import { useState } from 'react';
+import { configOptions } from '../constants/configOptions';
+
+export default function SingleSelectConfig() {
+    const dispatch = useDispatch();
+    const [selectedOption, setSelectedOption] = useState<{ [key: string]: string }>({});
+
+    const handleChange = (event: SelectChangeEvent) => {
+        const { name, value } = event.target;
+
+        setSelectedOption(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+
+        if (name === 'exp') {
+            dispatch(setExperience(Number(value)));
+        } else if (name === 'salary') {
+            const parsedValue = Number(value.replace(/[^0-9]/g, ''));
+            dispatch(setMinimumBasePaySalary(parsedValue));
+        }
+    };
+
+    return (
+        <div>
+        {configOptions.map((option, index) => (
+            <FormControl key={index} sx={{ m: 1, minWidth: `${option.width}px` }} size="small">
+            <InputLabel id={`select-label-${index}`}>{option.label}</InputLabel>
+            <Select
+                labelId={`select-label-${index}`}
+                value={selectedOption[option.key] || ''}
+                onChange={handleChange}
+                label={option.label}
+                name={option.key}
+            >
+                <MenuItem value="">
+                    <em>None</em>
+                </MenuItem>
+                {option.values.map((value, index) => (
+                    <MenuItem key={index} value={value} aria-label={value.toString()}>
+                        {value}
+                    </MenuItem>
+                ))}
+            </Select>
+            </FormControl>
+        ))}
+        </div>
+    );
+}
---
+++ src/constants/configOptions.ts
@@ -0,0 +1,20 @@
+export const configOptions = [
+    {
+        key: 'exp',
+        label: 'Experience',
+        values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        width: 150,
+    },
+    {
+        key: 'salary',
+        label: 'Minimum Base Pay Salary',
+        values: ['0L', '10L', '20L', '30L', '40L', '50L', '60L', '70L'],
+        width: 250,
+    },
+    {
+        key: 'location',
+        label: 'Location',
+        values: ['Remote', 'Office'],
+        width: 150,
+    }
+];
